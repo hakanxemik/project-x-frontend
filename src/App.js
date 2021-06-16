67af1019a56492cb8b93c99ea9833fae1346ed80
hakanxemik <hakan.emik@antiloop.com>
2021-06-16 09:45:01 +0200

    Merge with test/builder-x

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import "./icons.js";
 import Login from "./screens/Login";
 import SignUp from "./screens/SignUp";
