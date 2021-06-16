a6299161200c2db4752e96c946b1e1c3214fe2f2
Hakan Emik <47503046+hakanxemik@users.noreply.github.com>
2021-06-16 15:00:51 +0200

    SA-78 Happening Carousel with Card (#5)
    * Flipcard
    
    * deletes unused package
    
    * adds endpoint
    
    * fetch happenings from BE
    
    * asdf
    
    * Adds Carousel and Flip effect
    
    * Adds Happening Component Wrapper
    
    * Merge with test/builder-x
    
    * deletes unnecessary file
    
    * merge with develop
    
    * merge with develop and delete file
    
    * merge with dev
    
    Co-authored-by: Anil Celik <anilcelik@Anils-MacBook-Pro.local>

---
+++ src/components/HappeningCardBack.js
@@ -0,0 +1,99 @@
+import React, { Component, useState, useEffect } from "react";
+import { makeStyles } from '@material-ui/core/styles';
+
+
+import styled, { css } from "styled-components";
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+import StarOutlinedIcon from '@material-ui/icons/StarOutlined';
+import GroupOutlinedIcon from '@material-ui/icons/GroupOutlined';
+
+function HappeningCardBack(props) {
+
+    const useStyles = makeStyles(() => ({
+        content: {
+            padding: 24,
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            color: 'white'
+        },
+        cta: {
+            display: 'block',
+            textAlign: 'center',
+            color: '#fff',
+            letterSpacing: '3px',
+            fontWeight: 200,
+            fontSize: 12,
+        },
+        title: {
+            color: '#fff',
+            letterSpacing: '2px',
+            marginBottom: 0
+        },
+    
+        cardMedia: {
+            backgroundColor: `#2980b9`,
+            border: '20px solid #fff',
+            width: '64vw',
+            height: '430px',
+        }
+    }));
+
+    const styles = useStyles();
+
+    return (
+        <Card className={styles.cardMedia}>
+            <CardActionArea>
+                <CardContent className={styles.content}>
+                    <Box
+                        display={'flex'}
+                        flexDirection={'column'}
+                        alignItems={'center'}
+                        justifyContent={'center'}
+                        minHeight={360}
+                        color={'common.white'}
+                        textAlign={'center'}
+
+                    >
+                        <Box mb={3}>
+                            <Grid item xs={12}>
+                                <h1 className={styles.title}>{props.happening.title}</h1>
+                                <h3>25.06 um 18:00 Uhr <br />in Bludenz</h3>
+                            </Grid>
+                        </Box>
+
+                        <Box mb={3}>
+                            <Grid item>
+                                <AccountCircleOutlinedIcon style={{ verticalAlign: 'middle' }} fontSize={"large"}></AccountCircleOutlinedIcon>
+                                Example
+                            </Grid>
+                            <Grid item>
+                                <StarOutlinedIcon style={{ verticalAlign: 'middle' }} fontSize={"medium"}></StarOutlinedIcon>
+                            4.9
+                        </Grid>
+                        </Box>
+
+                        <Box my={3}>
+                            <Grid item>
+                                <GroupOutlinedIcon style={{ verticalAlign: 'middle' }} fontSize={"large"}></GroupOutlinedIcon>
+                                12 von 15 besetzt
+                            </Grid>
+                        </Box>
+                    </Box>
+                    <Typography className={styles.cta} variant={'overline'}>
+                        TAP FÜR VORDERSEITE
+                </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+}
+
+
+export default HappeningCardBack;
