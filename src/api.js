a30c583194e70ce2251a7cf6dc0563ad516c0941
hakanxemik <hakan.emik@antiloop.com>
2021-08-24 00:09:48 +0200

    update api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import { RepeatOneSharp } from "@material-ui/icons";
 import React from "react";
 
-const apiEndpoint = 'https://socialup-api.herokuapp.com/api';;
+const apiEndpoint = 'https://socialup-api.herokuapp.com/api';
 
 export async function getCategories() {
     const data = await fetch(apiEndpoint + '/categories')
